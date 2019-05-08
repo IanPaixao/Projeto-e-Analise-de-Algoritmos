4860fcee611287d9735312e49747950a0a9e6bc6
Ian Rodrigues dos Reis Paixão <36201596+IanPaixao@users.noreply.github.com>
2019-05-08 10:13:47 -0300

    Update StudiesOnDijkstra.cpp

--- StudiesOnDijkstra.cpp
+++ StudiesOnDijkstra.cpp
@@ -170,14 +170,15 @@ void Graph::DFS(int start, int end)
 
 int main()
 {
-Graph g(5);
-g.addEdge(0, 1, 2);
-g.addEdge(1, 2, 6);
+Graph g(4);
+g.addEdge(0, 1, 1);
+g.addEdge(0, 2, 100);
+g.addEdge(1, 2, 10);
+g.addEdge(1, 3, 100);
 g.addEdge(2, 3, 10);
-g.addEdge(3, 4, 8);
-g.addEdge(4, 0, 4);
-g.DFS(0, 4);
-int result = g.Dijkstra(0, 2);
+g.addEdge(3, 0, 1000);
+g.DFS(0, 3);
+int result = g.Dijkstra(0, 3);
 cout << result;
 
 return 0;
